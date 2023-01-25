8d9728235e1d1f58471abb7dc93c0b859fc2a5b7
nagarajuchallagulla <99012371+nagchallagulla@users.noreply.github.com>
2023-01-25 19:27:36 +0530

    Update list.js
    2nd commit

--- src/components/list.js
+++ src/components/list.js
@@ -1,12 +1,45 @@
-export const List=(item)=>{
+// export const List=(item)=>{
     
-            return (
-                <div class="card my-5 py-4" key={item.id} style={{width: "18rem"}}>
-                    <img src={item.image} class="card-img-top" alt={item.description}/>
-                        <div class="card-body text-center">
-                            <h5 class="card-title">{item.label}</h5>
-                        </div>
-    </div>
-                    );
-        }   
+//     return (
+//         <div class="card my-5 py-4" key={item.id} style={{width: "18rem"}}>
+//             <img src={item.image} class="card-img-top" alt={item.description}/>
+//                 <div class="card-body text-center">
+//                     <h5 class="card-title">{item.label}</h5>
+//                 </div>
+// </div>
+//             );
+// }   
+  
+
+
+function List({ searchResults, searchTerm }) {
+    console.log(searchTerm);
+    if (searchResults.length === 0){
+        console.log('no datafound');
+        return <h3 style={{color:'red'}}>No Matching Record Found!</h3>;
+
+    } 
+    return (
+        <div>
+            <div className='div2'>
+                    
+                <div className="count">Count : {searchResults.length} </div>
+            </div>
+            {
+                searchResults.map((item)=>{
+                    return(
+                        <div class="card my-5 py-4" key={item.id} style={{width: "18rem"}}>
+                                     <img src={item.image} class="card-img-top" alt={item.description}/>
+                                        <div class="card-body text-center">
+                                            <h5 class="card-title">{item.label}</h5>
+                                    </div>
+                                    </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
+export default List;
           
